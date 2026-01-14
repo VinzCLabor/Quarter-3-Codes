e2d8aa432482c917ceb7b3af39e49cdd0d5d5fd6
VinzCLabor <vinz.labor@smc.pshs.edu.ph>
2026-01-14 19:52:21 +0800

    Quarter 3 - FA 02

---
+++ Labor_Q3_FA02.py
@@ -0,0 +1,21 @@
+names = ["Vinz", "Lia", "Jake"]
+steps = [
+    [4500, 5200, 4800, 5000, 5300],
+    [4000, 4100, 3900, 4200, 4600],
+    [6000, 5800, 5900, 6100, 6200]
+]
+
+days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+
+
+print("Name".ljust(10), end="")
+for day in days:
+    print(day.ljust(10), end="")
+print()
+
+
+for i in range(len(names)):
+    print(names[i].ljust(10), end="")
+    for j in range(len(steps[i])):
+        print(str(steps[i][j]).ljust(10), end="")
+    print()
