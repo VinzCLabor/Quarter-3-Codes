e4db873b384fbef47c1a81230ae921e148a38dce
VinzCLabor <vinz.labor@smc.pshs.edu.ph>
2026-03-04 08:41:51 +0800

    Collect and store student information in a dictionary

--- Labor_Q3_FA06.py
+++ Labor_Q3_FA06.py
@@ -1,17 +1,14 @@
-# Create an empty dictionary named student
+
 student = {}
 
-# Use input() to collect data
 name = input("Enter your name: ")
 age = input("Enter your age: ")
 subject = input("Enter your favorite subject: ")
 
-# Store the data into the dictionary with specific keys
 student["name"] = name
 student["age"] = age
 student["subject"] = subject
 
-# Print the student's information in a clear format
 print("\nStudent Record:")
 print(f"Name: {student['name']}")
 print(f"Age: {student['age']}")
